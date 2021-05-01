bc7870500b3e22f1d1dc7781eb182c48a820ced2
saiph <saiph@localhost.localdomain>
2021-05-01 14:12:51 +0200

    add first json

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
+	@GetMapping
+	public List<Student> hello() {
+		return List.of(
+					new Student (
+							1L,
+							"Mariam",
+							"mariam.jamal@gmail.com",
+							LocalDate.of(1994, 12, 20),
+							21
+							)	
+					);
+	}
 }
